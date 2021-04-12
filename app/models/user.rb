fc3d0b14ba916782d13c4290f81653b1573d3942
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-04-12 16:29:59 +0900

    バリデーション直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password,:password_confirmation, length: {minimum:6}, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  validates :last_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+  validates :first_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
   validates :birthday, presence: true
 end
