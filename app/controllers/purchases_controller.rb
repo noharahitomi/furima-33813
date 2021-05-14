4c321ee542c768979b617120fbc9a7d1486188aa
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-05-14 23:25:51 +0900

    purchasesテーブル、addresseesテーブル作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,15 @@ class PurchasesController < ApplicationController
   end
 
   def create
-
+    binding.pry
+    @purchase_addresse = PurchaseAddressee.create(purchase_params)
+    if @purchase_addressee.valid?
+      @purchase_addressee.save
+      redirect_to root_path
+    end
   end
+
+  private
+  def purchase_params
+    params.require(:purshase_addressee).permit(:number, :cvc, :exp_month, :exp_year, :post_code, :prefecture_id, :municipality, :address, :building_name, :tel_number).merge(user_id: current_user.id, item_id: item.id)
 end
--- db/migrate/20210514135450_create_addressees.rb
+++ db/migrate/20210514135450_create_addressees.rb
@@ -1,7 +1,13 @@
 class CreateAddressees < ActiveRecord::Migration[6.0]
   def change
     create_table :addressees do |t|
-
+      t.integer  :post_code, null: false
+      t.string   :prefecture_id,  null: false
+      t.string   :municipality,   null: false
+      t.string   :address,        null: false
+      t.string   :building_name
+      t.integer  :tel_number,     null: false
+      t.references :purchase,     foreign_key: true
       t.timestamps
     end
   end
