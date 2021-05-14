4c831c8728e968ceb58e2bcf2231a760ebc3ab1d
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-05-14 22:49:02 +0900

    バリデーション、保存

--- app/models/purchase_addressees.rb
+++ app/models/purchase_addressees.rb
@@ -1,10 +1,17 @@
 class PurchaseAddressees
   include Activemodel::module
-    attr_accesser :post_code, :prefecture_id, :municipality, :address, :building_name, :tel_number, :purchase
+    attr_accesser :number, :cvc, :exp_month, :exp_year, :post_code, :prefecture_id, :municipality, :address, :building_name, :tel_number, :purchase
 
-    # ここにバリデーション
+    with_options presence: true do
+      validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+      validates :municipality, :address, :purchase
+      validates :tel_number, :number, :cvc, :exp_month, :exp_year,  numericality: {only_integer: true }
+    end
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
     def save
-      
+      purchase = Purchase.create(number: number, cvc: cvc, exp_month: exp_month, exp_year: exp_year)
+      Addressees.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, tel_number: tel_namuber, purchase: purchase)
     end
   end
+end
