83ae59b34d418d6fb9548d6ae75d70d3a8d9a32c
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-05-17 14:30:36 +0900

    テストコードの実装とバリデーションの修正2

--- spec/factories/purchase_addressee.rb
+++ spec/factories/purchase_addressee.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_addressee do
     post_code {'123-4567'}
-    prefecture_id {1}
+    prefecture_id {6}
     municipality { '横浜市' }
     address {'金沢1-3'}
     building_name {'コーポ横浜'}
